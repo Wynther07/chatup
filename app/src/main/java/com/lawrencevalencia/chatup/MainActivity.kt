4e7545c533278810ef6714c78c2dd7408c8a975e
Lawrence V <mlawrencevalencia@yahoo.com>
2020-03-23 23:05:36 -0400

    main screen objects

--- app/src/main/java/com/lawrencevalencia/chatup/MainActivity.kt
+++ app/src/main/java/com/lawrencevalencia/chatup/MainActivity.kt
@@ -2,6 +2,7 @@ package com.lawrencevalencia.chatup
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.View
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
@@ -38,5 +39,16 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    //Button onClick
+    fun loginBtnNavClicked (view:View){
+
+    }
+    fun addChannelBtnNavClicked (view:View){
+
+    }
+    fun sendMsgBtnClicked (view:View){
+
     }
 }
