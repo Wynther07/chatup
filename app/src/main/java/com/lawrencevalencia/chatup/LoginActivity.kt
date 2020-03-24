30581bdd8331cf09dc33c1483d8bd17c82f0091f
Lawrence V <mlawrencevalencia@yahoo.com>
2020-03-24 00:03:52 -0400

    created login screen and hooked up to main

---
+++ app/src/main/java/com/lawrencevalencia/chatup/LoginActivity.kt
@@ -0,0 +1,21 @@
+package com.lawrencevalencia.chatup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.lawrencevalencia.chatup.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    fun loginLoginBtnClicked (view:View) {
+
+    }
+    fun loginSignUpBtnClicked (view:View) {
+
+    }
+}
