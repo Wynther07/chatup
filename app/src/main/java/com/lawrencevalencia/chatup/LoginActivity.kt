a86dd6b638f5a7497190efec5c1d54be90a5face
Lawrence V <mlawrencevalencia@yahoo.com>
2020-03-24 00:57:56 -0400

    create user interface

--- app/src/main/java/com/lawrencevalencia/chatup/LoginActivity.kt
+++ app/src/main/java/com/lawrencevalencia/chatup/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.lawrencevalencia.chatup
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -16,6 +17,7 @@ class LoginActivity : AppCompatActivity() {
 
     }
     fun loginSignUpBtnClicked (view:View) {
-
+        val createUserIntent = Intent(this, CreateUserActivity::class.java)
+        startActivity(createUserIntent)
     }
 }
